dcbdc89786a388667f838f7c5caf17be25c4dac7
Makhmud Islamov <sunnatovichvv@gmail.com>
2019-01-31 21:34:34 -0800

    can submit username form via websocket

--- public/index.js
+++ public/index.js
@@ -1,4 +1,14 @@
 $(document).ready(() => {
-    //Connect to the socket.io server
     const socket = io.connect();
+
+    $('#createUserBtn').click((e) => {
+        e.preventDefault();
+        let username = $('#usernameInput').val();
+        if (username.length > 0) {
+            //Emit to the server the new user
+            socket.emit('new user', username);
+            $('.usernameForm').remove();
+        }
+    });
+
 })
--- sockets/chat.js
+++ sockets/chat.js
@@ -1,3 +1,8 @@
 module.exports = (io, socket) => {
-    //Future socket listeners will be here
+
+    // Listen for "new user" socket emits
+    socket.on('new user', (username) => {
+        console.log(`${username} has joined the chat! ✋`);
+    })
+
 }

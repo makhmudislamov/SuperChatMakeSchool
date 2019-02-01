a6242724d4911afca977c6197209145439b95902
Makhmud Islamov <sunnatovichvv@gmail.com>
2019-01-31 16:25:20 -0800

    socket.io on frontend

--- app.js
+++ app.js
@@ -12,6 +12,8 @@ io.on("connection", (socket) => {
 const exphbs = require('express-handlebars');
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
+//Establish your public folder
+app.use('/public', express.static('public'))
 
 app.get('/', (req, res) => {
     res.render('index.handlebars');
